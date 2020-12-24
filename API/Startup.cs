db61920fd5004b315d3f0e9cba9533c4a7eb92f2
kwizl <karis.njoroge@gmail.com>
2020-12-24 17:46:52 +0300

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,7 @@ namespace API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddCors();
       services.AddDbContext<DataContext>(options =>
       {
         options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
@@ -46,6 +47,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
