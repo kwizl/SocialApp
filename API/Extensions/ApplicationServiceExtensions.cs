eb416f2062fc701015ae5e849d2442fd81baf2fd
kwizl <karis.njoroge@gmail.com>
2021-02-18 21:53:02 +0300

    Create extension methods for services

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration onfig)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(onfig.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
